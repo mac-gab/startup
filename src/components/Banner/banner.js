8e0b951edecdd63d68792dff13a143c4b70193be
mac-gab <mac.gab@me.com>
2021-03-20 13:40:59 +0000

    banner.js: updated span

--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -38,7 +38,7 @@ const Banner = () => {
             variants={variants}
             transition={{ ease: "easeOut", duration: 0.8, delay: 1 }}
           >
-            It's time to make your business <span>staret out</span>
+            It's time to make your business <span>starte out</span>
           </motion.h1>
           <motion.p
             initial="hidden"
